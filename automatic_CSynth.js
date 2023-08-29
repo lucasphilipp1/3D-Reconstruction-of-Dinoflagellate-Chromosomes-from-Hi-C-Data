2617e94dae957a8138965936a45bd37267cd9508
lucasphilipp1 <94249076+lucasphilipp1@users.noreply.github.com>
2023-08-29 13:11:38 -0400

    updated some files

--- automatic_CSynth.js
+++ automatic_CSynth.js
@@ -1,24 +1,30 @@
-var {log, sleep, openfiles, CSynth, savshowDirectoryPicker, Files, saveimage, G, S, monitorX, GX, V} = window;  // globals from javascript and CSynth
+var {log, sleep, openfiles, CSynth, savshowDirectoryPicker, Files, saveimage, G, S, monitorX, GX, V, distxyz, springs} = window;  // globals from javascript and CSynth
+
+
+// rmse function on arrays of vectors
+CSynth.rmsev = (a,b) => Math.sqrt(a.reduce((c,v,i) => c + distxyz(a[i], b[i]), 0))
+
 
 /** process a single input txt file */
-async function process(file) {
+async function process(file, threshold = 0.1) {
     log('csyscript: realwork', file.name)
     await openfiles([await file.getFile()]);        // this will behave the same as dropping the single file
     await S.waitVal(() => CSynth.current.ready);    // wait till really started
 
-    GX.getgui('ribbon/diameter').setValue(30);
+
+    GX.getgui('ribbon/diameter').setValue(20);
+    GX.getgui(/beddatasource/).setValue('rainbow');
     G.stepsPerStep=50       // 50 simulation steps per graphics display, less wasted graphics effort, more GPU for simulation
     G.springrate = 10       // bigger simulation steps for faster convergence
     await sleep(3000)
     G.springrate = 1        // standard simulation steps for better stability
-    await sleep(120000) //120 seconds
-    CSynth.autoscale()
-    GX.getgui(/beddatasource/).setValue('rainbow')
+
+    
+    
     CSynth.showEigen(true);
     await S.frame()         // sleep till next frame (so eigen will have effect)
     log('csyscript: realwork done, saving', file.name)
-    GX.savegui('test.settings', true)
-    CSynth.savetxt(file.name + '.txt')
+    CSynth.savepdb(file.name + '.pdb')
     const sgui = V.gui.visible
     V.gui.visible = false;
     await saveimage(3000,2000, false, false, file.name + '.tga')
@@ -26,17 +32,21 @@ async function process(file) {
     log('csyscript: savingdone', file.name)
 }
 
+
 /** main test file */
 async function test() {
     const dirhandle = Files.dirhandle || await Files.setDirectory();           // this will establish the Files local directory to work in
     const nts = dirhandle.entries();
 
+
     log('csyscript: starting');
+
 
     for await (const [name, file] of nts)
         if (name.endsWith('.txt')) await process(file);
     log('csyscript: all files done');
 }
+
 
 /** looping test file for debug, generally use test() directly */
 async function tests(n = 10) {
@@ -45,5 +55,6 @@ async function tests(n = 10) {
         Files.write('progress' + i, 'done')
     }
 }
+
 
 test()

efc4b7a9f8d773d51b346528f5ac6e6a4f2a4b69
lucasphilipp1 <94249076+lucasphilipp1@users.noreply.github.com>
2023-09-11 17:49:23 -0400

    intrachromosomal loops variable size

--- constrained_self_avoiding_RW_3D.m
+++ constrained_self_avoiding_RW_3D.m
@@ -20,7 +20,11 @@ while isunique==0
     end
     walk = [x y z];
 
-    if steps > 50
+    if steps > 100
+        if size(unique(walk, "rows"),1) >= steps*0.90
+            isunique=1;
+        end
+    elseif steps > 50
         if size(unique(walk, "rows"),1) >= steps*0.95
             isunique=1;
         end

48205de3a85fec173f14b95f1aeac7f14cb8fb44
lucasphilipp1 <94249076+lucasphilipp1@users.noreply.github.com>
2025-09-22 11:33:51 -0400

    missing script in folder
    contact_probability_xyz.m is required by scripts in multiple folders

---
+++ 3. Simulating CLCs/contact_probability_xyz.m
@@ -0,0 +1,17 @@
+%see: Naumova, N., Imakaev, M., Fudenberg, G., Zhan, Y., Lajoie, B. R., Mirny, L. A., & Dekker, J. (2013). Organization of the mitotic chromosome. Science, 342(6161), 948-953.
+function [s Ps] = contact_probability_xyz(model,distance_threshold)
+Ps=zeros(size(model,1)-1,1);
+s=1:1:size(model,1)-1;
+s=s';
+D = pdist(model);
+D = squareform(D);
+cont_count_mat = D;
+cont_count_mat(cont_count_mat<distance_threshold) = 0;
+cont_count_mat(cont_count_mat>distance_threshold) = 1;
+cont_count_mat(:) = ~cont_count_mat;
+cont_count=zeros(1,size(model,1)-1);
+for i=1:1:size(model,1)-1
+    cont_count(i) = sum(diag(cont_count_mat,i));
+    Ps(i)=cont_count(i)/(size(model,1)-i);
+end
+end
